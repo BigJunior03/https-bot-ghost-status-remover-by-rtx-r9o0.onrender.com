c5141256751fa0e64d16e57463516cfd635ed82c
BigJunior03 <164936802+BigJunior03@users.noreply.github.com>
2024-03-25 14:53:47 -0500

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Watching over the Shop"];
 
 
 let currentIndex = 0;
